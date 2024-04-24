68718eb8e3bcdb49afb62617dc950d493cde260d
TheJuanRojasR <juan_mrojasr@soy.sena.edu.co>
2024-04-23 20:14:56 -0500

    8th commit : Se crean todas las peticiones de la entidad INFORMES.
    Se crean todas las peticiones.
    Se tiene que corregir.
    Colocar variables en ingles.

--- src/controllers/informesController.js
+++ src/controllers/informesController.js
@@ -1,7 +1,7 @@
 const Informe = require(  '../models/informes' );
 
 module.exports = {
-    mostrar( req, res ) {
+    getSalesByProduct( req, res ) {
         const id_producto = req.query.id_producto || null; // Id del producto
         const fecha_inicio = req.query.fecha_inicio || null; // Fecha del informe
         const fecha_fin = req.query.fecha_fin || null; // Fecha del informe
@@ -27,7 +27,7 @@ module.exports = {
             fecha_fin: fecha_fin
         };
 
-        Informe.mostrar( informe, ( err, data ) => {
+        Informe.getSalesByProduct( informe, ( err, data ) => {
             if( err ) {
                 res.status( 501 ).json({
                     success: false,
@@ -42,5 +42,69 @@ module.exports = {
             });
         });
     },
+
+    getTopSalesByCategory( req, res ) {
+        const fecha_inicio = req.query.fecha_inicio || null; // Fecha del informe
+        const fecha_fin = req.query.fecha_fin || null; // Fecha del informe
+
+        if( !fecha_inicio || !fecha_fin ) {
+            return res.status( 400 ).json({
+                success: false,
+                message: 'Las fechas son requeridas'
+            });
+        }
+
+        const informe = {
+            fecha_inicio: fecha_inicio,
+            fecha_fin: fecha_fin
+        };
+
+        Informe.getTopSalesByCategory( informe, ( err, data ) => {
+            if( err ) {
+                res.status( 501 ).json({
+                    success: false,
+                    message: 'Error al leer los informes',
+                    error: err
+                });
+            }
+            return res.status( 200 ).json({
+                success: true,
+                message: res.message,
+                data: data // Datos de los informes
+            });
+        });
+    },
+
+    getSalesByStore( req, res ) {
+        const id_almacen = req.query.id_almacen || null; // Id del producto
+
+        if( !id_almacen ) {
+            return res.status( 400 ).json({
+                success: false,
+                message: 'El id del almacen es requerido'
+            });
+        }
+
+        const informe = {
+            id_almacen : id_almacen,
+        };
+
+        Informe.getSalesByStore( informe, ( err, data ) => {
+            if( err ) {
+                res.status( 501 ).json({
+                    success: false,
+                    message: 'Error al leer los informes',
+                    error: err
+                });
+            }
+            return res.status( 200 ).json({
+                success: true,
+                message: res.message,
+                data: data // Datos de los informes
+            });
+        });
+    }
+
+
 }
 
