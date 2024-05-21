06027b2035d382fb1b65eb9275ae653edb4ac4cf
TheJuanRojasR <160604613+TheJuanRojasR@users.noreply.github.com>
2024-05-21 09:52:38 -0500

    MORGAN y CORS
    Se desístala MORGAN por posible conflictos.
    Se instala CORS para la conexión de de cliente servidor.

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
+
+app.use = (cors());
+
+// app.use ((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+// });
 
 //settings
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
 
 //middlewares
-app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
