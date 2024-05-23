318e77babb9e6df61abaaaa421e6d03f48cd3767
TheJuanRojasR <160604613+TheJuanRojasR@users.noreply.github.com>
2024-05-23 10:00:28 -0500

    Comentarios Rutas usuarios
    Se agregan los comentarios de funcionalidad de cada ruta pala la entidad Usuarios

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -2,13 +2,13 @@ const { Router } = require('express');
 const router = Router(); //routes
 const usuariosController = require('../controllers/usuariosController');
 
-router.post('/usuarios/registro', usuariosController.register); //
-router.post('/usuarios/login', usuariosController.login);
-router.post('/usuarios/perfiles/registro', usuariosController.register);
-router.get('/usuarios/perfiles', usuariosController.getAllPerfil);
-router.put('/usuarios/perfiles', usuariosController.updatePerfil);
-router.put('/usuarios/perfiles/eliminar', usuariosController.deletePerfil);
-router.get('/usuarios/ajustes', usuariosController.getUsuario);
-router.put('/usuarios/ajustes', usuariosController.updateUsuario);
+router.post('/usuarios/registro', usuariosController.register); // Ruta para registrar un usuario
+router.post('/usuarios/login', usuariosController.login); // Ruta para el login de un usuario
+router.post('/usuarios/perfiles/registro', usuariosController.register); // Ruta para registrar un perfil
+router.get('/usuarios/perfiles', usuariosController.getAllPerfil); 
+router.put('/usuarios/perfiles', usuariosController.updatePerfil); // Ruta para actualizar un perfil
+router.put('/usuarios/perfiles/eliminar', usuariosController.deletePerfil); // Ruta para eliminar un perfil
+router.get('/usuarios/ajustes', usuariosController.getUsuario); //Ruta para obtener la información de un usuario
+router.put('/usuarios/ajustes', usuariosController.updateUsuario); // Ruta para actualizar la información de un usuario
 
 module.exports = router;
